1c818bf7ff6fe7d9ae06c8487298fcef4a4fd748
duongdv95 <danielvanduong@gmail.com>
2018-09-14 19:54:04 +0000

    Add update todo

--- public/index.js
+++ public/index.js
@@ -2,6 +2,25 @@ const createUser = document.querySelector(".create-user");
 const loginUser = document.querySelector(".login-user");
 const createTodo = document.querySelector(".todo-form");
 const todoList = document.querySelector(".todo-list");
+var editButton = (function() {
+    var button;
+    var state = false;
+    return {
+        store: function(element) {
+            button = element;
+            state = true;
+        },
+        getElement: function() {
+            return button;
+        },
+        exists: function() {
+            return state;
+        },
+        clear: function() {
+            state = false;
+        }
+    }
+})();
 
 createUser.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -22,12 +41,53 @@ loginUser.addEventListener("submit", (e) => {
 
 createTodo.addEventListener("submit", (e) => {
     e.preventDefault();
-    const todoText = createTodo.querySelector(".todo").value;
+    const todoText = createTodo.querySelector(".todo-input").value;
     if(todoText) {
-        var newTodoElement = `<div>${todoText}</div>`;
+        var newTodoElement = `<div class="todo">
+                                   <div>
+                                        ${todoText}
+                                   </div>
+                                   <div>
+                                        <input type="submit" value="edit" class="edit-todo-button"></input>
+                                        <input type="submit" value="x" class="delete-todo-button"></input>
+                                   </div>
+                              </div>`;
         todoList.insertAdjacentHTML("beforeend", newTodoElement);
     }
-    createTodo.querySelector(".todo").value = "";
+    createTodo.querySelector(".todo-input").value = "";
+})
+
+document.addEventListener("click", function(e) {
+    if(e.target && e.target.className == "delete-todo-button"){
+        var getTodo = e.target.parentElement.parentElement;
+        getTodo.remove();
+    }
+})
+
+document.addEventListener("click", function(e) {
+    if(!editButton.exists()) {
+        if(e.target && e.target.className == "edit-todo-button"){
+            editButton.store(e.target);
+            editButton.getElement().classList.add("hide-edit-todo-button");
+            var todoElement = e.target.parentElement.previousElementSibling;
+    		var getTodo = todoElement.innerHTML.trim();
+    	    todoElement.innerHTML = `<form>
+    	                                  <input type="text" value="${getTodo}"></input><input type="submit" value="finish" class="update-todo-button"></input>
+    	                             </form>`;
+        }
+    }
+
+})
+
+document.addEventListener("click", function(e) {
+    if(e.target && e.target.className == "update-todo-button"){
+        editButton.getElement().classList.remove("hide-edit-todo-button");
+        editButton.clear();
+        var todoElement = e.target.previousElementSibling;
+		var getTodo = todoElement.value.trim();
+		console.log(getTodo);
+	    todoElement.parentElement.parentElement.innerHTML = `${getTodo}`;
+    }
 })
 
 function post (path, data) {
