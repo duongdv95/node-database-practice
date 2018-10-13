ab8191900a3934306b9385bcdd38dd149eba5072
duongdv95 <danielvanduong@gmail.com>
2018-10-13 12:43:18 +0000

    Switch variables in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ const config = require("./config.js");
 module.exports = {
   client: 'mysql',
   connection: {
-    host: config.db_host.HOST || process.env.DBHOST,
-    port: config.db_host.PORT || process.env.DBPORT,
-    user: config.db_host.USER || process.env.DBUSER,
-    password: config.db_host.PASSWORD || process.env.DBPASSWORD,
-    database: config.db_host.DATABASE  || process.env.DBDATABASE
+    host: process.env.DBHOST || config.db_host.HOST,
+    port: process.env.DBPORT || config.db_host.PORT,
+    user: process.env.DBUSER || config.db_host.USER,
+    password: process.env.DBPASSWORD || config.db_host.PASSWORD,
+    database: process.env.DBDATABASE || config.db_host.DATABASE
   }
 }
